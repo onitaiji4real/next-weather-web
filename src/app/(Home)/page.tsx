60dd230f60cc1e7884fa8e72132e56dfddef5623
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-28 19:47:18 +0800

    feat: add Layout

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,10 +1,11 @@
 import TheHeader from "@/layout/TheHeader";
 import React from "react";
+import Layout from "@/layout/Layout";
 
 export default function page() {
   return (
     <>
-      <TheHeader />
+      <Layout />
     </>
   );
 }
