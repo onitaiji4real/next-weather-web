61a3b3c2c422714f640327a191b6a8b1178486d7
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-28 19:50:05 +0800

    feat: add HourlyWeather type

---
+++ src/types/hourlyWeather.ts
@@ -0,0 +1,10 @@
+export type HourlyWeather = {
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+    relative_humidity_2m: number[];
+    weather_code: number[];
+    apparent_temperature: number[]; // 表觀溫度
+    wind_speed_10m: number[]; //風速10m
+  };
+};
