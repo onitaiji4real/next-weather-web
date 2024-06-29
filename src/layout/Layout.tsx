8e0435e0a1e3e2aad128471a9b62067d73b86f4c
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-29 22:48:16 +0800

    feat: add fetchWeather for a origin location

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,15 +2,21 @@ import React, { useEffect } from "react";
 import TheHeader from "./TheHeader";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import TheMain from "./TheMain";
-import { useDispatch } from "react-redux";
-import { fetchWeather } from "@/app/(Home)/weatherSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWeather, HourlyWeatherSlice } from "@/app/(Home)/weatherSlice";
 import type { AppDispatch } from "@/app/(Home)/store";
 
 export default function Layout() {
   const dispatch = useDispatch<AppDispatch>();
+  const data = useSelector(HourlyWeatherSlice);
+  const longitude = data.longitude;
+  const latitude = data.latitude;
+
   useEffect(() => {
-    dispatch(fetchWeather());
+    // dispatch(fetchWeather({ latitude: 22.6266, longitude: 120.3613 }));
+    dispatch(fetchWeather({ latitude, longitude }));
   }, [dispatch]);
+
   return (
     <>
       <TheHeader />
