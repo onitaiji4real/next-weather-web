23724ea0cb64f2b3729019281e3fd3ccf50e06ce
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-29 17:16:45 +0800

    feat: add to use weatherSlice

--- src/components/LocalWeatherInfo.tsx
+++ src/components/LocalWeatherInfo.tsx
@@ -1,40 +1,42 @@
 "use client";
 import React from "react";
 import { LocalTemperature } from "./LocalTemperature";
-import { HourlyWeather } from "@/types/hourlyWeather";
 import useSWR from "swr";
 import { weatherFetcher } from "@/lib/utils/weatherFetcher";
 import { getHightestTemp, getLowtestTemp } from "@/lib/utils/basicTempAPI";
-import { getCurrentWeather } from "@/lib/utils/commomAPI";
+import { getCurrentHourIndex, getCurrentWeather } from "@/lib/utils/commomAPI";
+import { useDispatch, useSelector } from "react-redux";
+import { HourlyWeatherSlice } from "@/app/(Home)/weatherSlice";
+import { getWeatherCodeDescription } from "@/lib/utils/wmoWeatherCodeMap";
+import Image from "next/image";
 
 export default function LocalWeather() {
-  const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=22.6266&longitude=120.3613&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms&forecast_days=1";
+  const data = useSelector(HourlyWeatherSlice);
+  const currentTemp = data.hourly.apparent_temperature[getCurrentHourIndex()];
+  const todayHightestTemp = getHightestTemp(data.hourly.apparent_temperature);
+  const todatLowtestTemp = getLowtestTemp(data.hourly.apparent_temperature);
 
-  const { data, isLoading, error } = useSWR<HourlyWeather>(url, weatherFetcher);
+  const weatherCode = data.hourly.weather_code[getCurrentHourIndex()];
 
-  const hightestTemp = data
-    ? getHightestTemp(data.hourly.apparent_temperature)
-    : "--";
-  const lowtestTemp = data
-    ? getLowtestTemp(data.hourly.apparent_temperature)
-    : "--";
-
-  const currentWeather = data
-    ? getCurrentWeather(data)
-    : { temperature: "--", humidity: "--", windSpeed: "--" };
+  const weatherDescription =
+    weatherCode !== undefined
+      ? getWeatherCodeDescription(weatherCode.toString())
+      : { description: "Unknown Weather Code", image: "" };
 
   return (
     <>
-      <div className="w-full min-h-60 bg-slate-400 border rounded-xl flex flex-col items-center p-5 gap-1">
+      <div className="w-full min-h-60 bg-slate-400 border rounded-xl flex flex-col items-center p-5 gap-2">
         <h2 className="text-3xl">My Location</h2>
         <h6>鳳山區</h6>
-        <p className=" text-8xl">{`${currentWeather.temperature}°`}</p>
+        <p className=" text-8xl">{`${currentTemp}°`}</p>
         <div className="flex justify-center items-center gap-8">
           <LocalTemperature
-            hightestTemp={hightestTemp}
-            lowtestTemp={lowtestTemp}
+            hightestTemp={todayHightestTemp}
+            lowtestTemp={todatLowtestTemp}
           />
+        </div>
+        <div className="flex justify-center items-center text-3xl pt-5 ">
+          {weatherDescription.description}
         </div>
       </div>
     </>
--- src/components/LocalWindSpeed.tsx
+++ src/components/LocalWindSpeed.tsx
@@ -1,19 +1,21 @@
+"use client";
 import React from "react";
 import { SendHorizontal } from "lucide-react";
 import { WindDirection } from "./WindSpeed/WindDirection";
 import { WindSpeed } from "./WindSpeed/WindSpeed";
+import { useSelector } from "react-redux";
 
 interface WindProps {
   windDirection: number;
 }
 
 export const LocalWindSpeed = ({ windDirection }: WindProps) => {
-  //   const adjustedDirection = windDirection;
-
   return (
-    <div className=" bg-slate-400 w-[50%] min-h-60 border rounded-xl flex justify-around items-center">
-      <WindSpeed />
-      <WindDirection windDirection={90} />
-    </div>
+    <>
+      <div className=" bg-slate-400 w-[50%] min-h-60 border rounded-xl flex justify-around items-center">
+        <WindSpeed />
+        <WindDirection windDirection={0} />
+      </div>
+    </>
   );
 };
