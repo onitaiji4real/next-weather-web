9136645a603609a9865810eb1d02e7d63813beca
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-29 17:15:37 +0800

    feat: add to use weatherSlice

--- src/components/WindSpeed/WindDirection.tsx
+++ src/components/WindSpeed/WindDirection.tsx
@@ -1,18 +1,27 @@
+import { HourlyWeatherSlice } from "@/app/(Home)/weatherSlice";
+import { getCurrentHourIndex } from "@/lib/utils/commomAPI";
 import { SendHorizontal } from "lucide-react";
 import React from "react";
+import { useSelector } from "react-redux";
 
 interface WindProps {
   windDirection: number;
 }
 
 export const WindDirection = ({ windDirection }: WindProps) => {
+  const data = useSelector(HourlyWeatherSlice);
+
+  const currentWindDirection = data
+    ? data.hourly.wind_direction_10m[getCurrentHourIndex()]
+    : 0;
+
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-2">
       <SendHorizontal
         size={100}
-        style={{ transform: `rotate(${windDirection - 90}deg)` }}
+        style={{ transform: `rotate(${currentWindDirection - 90}deg)` }}
       />
-      <h2 className="text-5xl">{`${windDirection}°`}</h2>
+      <h2 className="text-5xl">{`${currentWindDirection}°`}</h2>
     </div>
   );
 };
--- src/components/WindSpeed/WindSpeed.tsx
+++ src/components/WindSpeed/WindSpeed.tsx
@@ -1,10 +1,23 @@
+import { HourlyWeatherSlice } from "@/app/(Home)/weatherSlice";
+import {
+  getCurrentHourIndex,
+  getWindSpeedToBeaufort,
+} from "@/lib/utils/commomAPI";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const WindSpeed = () => {
+  const data = useSelector(HourlyWeatherSlice);
+
+  const currentWindSpeed = data.hourly.wind_speed_10m[getCurrentHourIndex()];
+  const currentWindLevel = getWindSpeedToBeaufort(
+    data.hourly.wind_speed_10m[getCurrentHourIndex()]
+  );
+
   return (
     <div className="flex flex-col gap-5">
       <div className="flex justify-start items-center gap-3">
-        <h2 className="text-5xl">{`3`}</h2>
+        <h2 className="text-5xl">{currentWindLevel}</h2>
         <div>
           <p>級</p>
           <p>風力</p>
@@ -12,7 +25,7 @@ export const WindSpeed = () => {
       </div>
       <hr />
       <div className="flex justify-start items-center gap-3">
-        <h2 className="text-5xl">{`3`}</h2>
+        <h2 className="text-5xl">{currentWindSpeed}</h2>
         <div>
           <p>公尺/秒</p>
           <p>風速</p>
