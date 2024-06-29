884adaf7bdd083cd8fb236bbba5d834d96ac0ae1
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-29 17:12:43 +0800

    feat: add dispatch to initial weatherData

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TheHeader from "./TheHeader";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import TheMain from "./TheMain";
+import { useDispatch } from "react-redux";
+import { fetchWeather } from "@/app/(Home)/weatherSlice";
+import type { AppDispatch } from "@/app/(Home)/store";
 
 export default function Layout() {
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(fetchWeather());
+  }, [dispatch]);
   return (
     <>
       <TheHeader />
