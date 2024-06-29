1a6fe39ff68e07c4398d3a2f384667cb137fd32b
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-29 17:13:44 +0800

    feat: add type wind_direction & dew_point

--- src/types/hourlyWeather.ts
+++ src/types/hourlyWeather.ts
@@ -6,5 +6,7 @@ export type HourlyWeather = {
     weather_code: number[];
     apparent_temperature: number[]; // 表觀溫度
     wind_speed_10m: number[]; //風速10m
+    wind_direction_10m: number[];
+    dew_point_2m: number[];
   };
 };
