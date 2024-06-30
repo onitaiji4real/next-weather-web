03c63de27607d8a8c64042c9cf297d8436df99d3
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-30 18:22:50 +0800

    feat: add future type

--- src/types/hourlyWeather.ts
+++ src/types/hourlyWeather.ts
@@ -1,17 +1,39 @@
+export type WeatherData = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  hourly_units: {
+    time: string;
+    temperature_2m?: string;
+    relative_humidity_2m?: string;
+    dew_point_2m?: string;
+    apparent_temperature: string;
+    weather_code: string;
+    wind_speed_10m?: string;
+    wind_direction_10m?: string;
+  };
+  hourly: {
+    time: string[];
+    temperature_2m?: number[];
+    relative_humidity_2m?: number[];
+    dew_point_2m?: number[];
+    apparent_temperature: number[];
+    weather_code: number[];
+    wind_speed_10m?: number[];
+    wind_direction_10m?: number[];
+  };
+};
+
 export type HourlyWeather = {
   isLoading: boolean;
   latitude: number;
   longitude: number;
   country: string;
   name: string;
-  hourly: {
-    time: string[];
-    temperature_2m: number[];
-    relative_humidity_2m: number[];
-    weather_code: number[];
-    apparent_temperature: number[]; // 表觀溫度
-    wind_speed_10m: number[]; //風速10m
-    wind_direction_10m: number[];
-    dew_point_2m: number[];
-  };
+  hourly: WeatherData["hourly"];
+  future: WeatherData["hourly"];
 };
