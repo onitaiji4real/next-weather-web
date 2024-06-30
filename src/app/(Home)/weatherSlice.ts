7b2053d2b8354e66d73c49d70e43ef4055a6f5e6
Hank_Chen <hank_chen@rosati.com.tw>
2024-06-30 18:23:17 +0800

    feat: add fetch future weather data

--- src/app/(Home)/weatherSlice.ts
+++ src/app/(Home)/weatherSlice.ts
@@ -19,14 +19,28 @@ const initialState: HourlyWeather = {
     wind_direction_10m: [],
     dew_point_2m: [],
   },
+  future: {
+    time: [],
+    apparent_temperature: [],
+    weather_code: [],
+  },
 };
 
 export const fetchWeather = createAsyncThunk(
   "weather/fetchWeather",
   async ({ latitude, longitude }: { latitude: number; longitude: number }) => {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,,dew_point_2m,apparent_temperature,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms&forecast_days=1`;
-    const response = await axios.get(url);
-    return response.data;
+    const hourlyUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,,dew_point_2m,apparent_temperature,weather_code,wind_speed_10m,wind_direction_10m&wind_speed_unit=ms&forecast_days=1`;
+    const futureUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=apparent_temperature,weather_code`;
+
+    const [hourlyResponse, futureResponse] = await Promise.all([
+      axios.get(hourlyUrl),
+      axios.get(futureUrl),
+    ]);
+
+    return {
+      hourlyData: hourlyResponse.data,
+      futureData: futureResponse.data,
+    };
   }
 );
 
@@ -56,7 +70,8 @@ const weatherSlice = createSlice({
       })
       .addCase(fetchWeather.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.hourly = action.payload.hourly;
+        state.hourly = action.payload.hourlyData.hourly;
+        state.future = action.payload.futureData.hourly;
       })
       .addCase(fetchWeather.rejected, (state) => {
         state.isLoading = false;
